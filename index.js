2edf71855c104c9c63a862803add26f42457dde9
Hafidhuu <masudhhaji@gmail.com>
2020-07-18 06:43:01 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,72 @@
+// name = "Hello, Masudh!";
+console.log("Hello, Masudh!");
+
+// let birthDate = "2020 - 1990";
+// console.log("2020 - 1990 equals", 2020 - 1990, "years");
+
+let name = "Masudh";
+let age = 30;
+
+console.log(name);
+console.log(age);
+
+let Square = "Length";
+console.log("Length");
+
+let lenght = 10;
+console.log(10);
+
+let circle = "Rdius";
+console.log("Redius");
+
+let radius = 15;
+console.log(15);
+
+let travel = "Distance";
+console.log("Distance");
+
+let distance = 578,
+  kilometer;
+console.log(528, kilometer);
+
+let time = "Hours";
+console.log("Hours");
+
+let hours = 5;
+console.log(5);
+
+let travell = "Speed";
+console.log("Speed");
+
+console.log("578 kilometer / 5 hours", 578 / 5);
+console.log("Speed = 115.6/hr");
+
+let price = "100 Dollars = 112 Euros";
+
+console.log("100 Dollars = 112 Euros");
+
+let Dollar = 100;
+let Euro = 112;
+
+console.log("Dollar cost less than Euro", Dollar < Euro);
+
+let item = "flashDrive";
+let flashDrive = "16Gigabytes";
+item = "file";
+let file = "820Megabytes";
+
+console.log("flashDrive");
+console.log("16Gigabytes");
+console.log("File");
+console.log("16000 / 820", 16 / 820, "MB");
+
+let didgit = 986;
+console.log(986);
+console.log("986 / 100 equals", 986 / 100, "%");
+
+let amount = "Deposit";
+let Deposit = 360000;
+
+console.log("Deposit");
+console.log(36000);
+console.log("360000 ** 5 / 100 equals", 360000 ** 5 / 100);
